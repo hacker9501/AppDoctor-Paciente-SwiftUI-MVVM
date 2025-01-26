92a899798103bf1384422544fbede1341e9c160c
Elver <holahacker01@gmail.com>
2025-01-26 12:51:50 -0500

    Excluye xcuserdata del seguimiento de Git

---
+++ Doctor/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Doctor
+//
+//  Created by Elver Mayta Hernández on 20/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Home()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
